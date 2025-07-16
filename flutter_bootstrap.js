0da4381d6a2c68fdb88e3664d1a2eddcf695b4d9
pavankumarswamy <shesettipavankumarswamy@gmail.com>
2025-07-16 07:53:25 +0530

    Update web build with --no-tree-shake-icons and hardcoded base href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2991982402"
+    serviceWorkerVersion: "2661828012"
   }
 });
